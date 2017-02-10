cf4bcf1b5b777991eebfda34df1db984a054c878
Johan <user.name>
2017-02-10 09:16:38 +0100

    fix: add crossorigin true to loader

--- app/states/loading-state.js
+++ app/states/loading-state.js
@@ -4,6 +4,7 @@ export class LoadingState extends Phaser.State {
   preload() {
     let loader = this.add.image(this.world.centerX, this.world.centerY, 'loader')
     loader.anchor.set(0.5, 0.5)
+    loader.crossOrigin(true)
     this.load.setPreloadSprite(loader)
 
     this.load.image('player', 'assets/images/player.png')
